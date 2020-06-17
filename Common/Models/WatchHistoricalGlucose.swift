f378e97229b327ec4688c267495167578eac537a
Test-4860 <67047793+Test-4860@users.noreply.github.com>
2020-06-17 15:47:17 +0200

    Update WatchHistoricalGlucose.swift

--- Common/Models/WatchHistoricalGlucose.swift
+++ Common/Models/WatchHistoricalGlucose.swift
@@ -16,7 +16,7 @@ struct WatchHistoricalGlucose {
 
     init(with samples: [StoredGlucoseSample]) {
         self.samples = samples.map {
-            NewGlucoseSample(date: $0.startDate, quantity: $0.quantity, isDisplayOnly: false, syncIdentifier: $0.syncIdentifier, syncVersion: 0)
+            NewGlucoseSample(date: $0.startDate, quantity: $0.quantity, isDisplayOnly: $0.isDisplayOnly, wasUserEntered: $0.wasUserEntered, syncIdentifier: $0.syncIdentifier, syncVersion: 0)
         }
     }
 }
@@ -29,7 +29,9 @@ extension WatchHistoricalGlucose: RawRepresentable {
         return [
             "d": samples.map { $0.date },
             "v": samples.map { Int16($0.quantity.doubleValue(for: .milligramsPerDeciliter)) },
-            "id": samples.map { $0.syncIdentifier }
+            "id": samples.map { $0.syncIdentifier },
+            "do": samples.map { $0.isDisplayOnly },
+            "ue": samples.map { $0.wasUserEntered }
         ]
     }
 
@@ -39,13 +41,15 @@ extension WatchHistoricalGlucose: RawRepresentable {
             let values = rawValue["v"] as? [Int16],
             let syncIdentifiers = rawValue["id"] as? [String],
             dates.count == values.count,
-            dates.count == syncIdentifiers.count
+            dates.count == syncIdentifiers.count,
+            dates.count == isDisplayOnly.count,
+            dates.count == wasUserEntered.count
         else {
                 return nil
         }
 
         self.samples = (0..<dates.count).map {
-            NewGlucoseSample(date: dates[$0], quantity: HKQuantity(unit: .milligramsPerDeciliter, doubleValue: Double(values[$0])), isDisplayOnly: false, syncIdentifier: syncIdentifiers[$0], syncVersion: 0)
+            NewGlucoseSample(date: dates[$0], quantity: HKQuantity(unit: .milligramsPerDeciliter, doubleValue: Double(values[$0])), isDisplayOnly: isDisplayOnly[$0], wasUserEntered: wasUserEntered[$0], syncIdentifier: syncIdentifiers[$0], syncVersion: 0)
         }
     }
 }
